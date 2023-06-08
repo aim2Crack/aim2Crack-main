bb76fc92a10e0bc478db23d5e1ae16709414468f
Prerna <divyadudhekar13@gmail.com>
2023-06-08 21:22:24 +0530

    SignUp.js work is going on

--- client/src/modules/quiz/creator/createQuiz/SignUp.js
+++ client/src/modules/quiz/creator/createQuiz/SignUp.js
@@ -1,38 +1,28 @@
 import React from 'react'
-// import prerna from './prerna.png';
 
-const SignUp =()=>{
+const SignUp=()=>{
     return(
-        <>
-        <div id="outer_div" >
-            <div id="div1">
-            <form>
+        <div id="outer_div">
+        <div id="div1">
+        <form>
                 <h1>Create Your Own Quiz</h1>
-            
                 <label for="starting_time">Start Time:</label>
                 <input className="inputbox" type="date" id="starting_time"/><br/>
                 <label for="last_login_time">Last login Time:</label>
                 <input className="inputbox" type="date" id="last_login_time"/><br/>
                 <label for="Result_time">Result Time:</label>
                 <input className="inputbox" type="date" id="Result_time"/><br/>
-               
                 <p id="para">Question Timer: </p>
-                
-                <select id="para" name="question_time">
+                <select name="question_time">
                 <option selected>Individual Question Timer</option>
                 <option >Section/Full Quiz Timer</option>
         
-                </select><br/>
-                
-                <button className='button' type="button">Create Quiz</button>
-                
+                </select><br></br>
+                <button class="button" type="button">Create Quiz</button>
                 
             </form>
-
-            </div>
+   </div>
         </div>
-        </>
     )
 }
-
 export default SignUp;
