5d082ac7f310f06cc15a2e1b5deb6ebbe11fa51c
Prerna <divyadudhekar13@gmail.com>
2023-06-09 18:06:27 +0530

    createquiz.jsx upload1

--- client/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
+++ client/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const createquiz=()=>{
-    return(
-        <div id="outer_div">
+const SignUp=()=>{
+    const [selectedOption, setSelectedOption] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    
+
+    const [labelText, setLabelText] = useState('Course Quiz :');
+    const [labelText2,setLabelText2] = useState('Topic name :')
+  
+    const handleSelect = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedOption(selectedValue);
+    setInputValue('');
+
+
+    // Update the input value and label text based on the selected option
+    if (selectedValue === 'placement') {
+      setInputValue('');
+      setLabelText('Company Name:');
+      setLabelText2('First section name');
+    } else if (selectedValue === 'simple') {
+      setInputValue('');
+      setLabelText('Course Quiz:');
+      setLabelText2('Topic name')
+    }
+  };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    
+  };
+
+
+  return(
+    <div id="outer_div">
         <div id="div1">
         <form>
                 <h1 className='heading'>Create Your Own Quiz</h1>
-                <label className="inputbox" for="starting_time">Start Time:</label>
+        <p className='select'>Type of Quiz : </p>        
+        
+      <select className="custom-select" value={selectedOption} onChange={handleSelect}>
+        <option value="">Select an option</option>
+        <option value="placement">Placement Quiz</option>
+        <option value="simple">Simple Quiz</option>
+      </select><br/>
+
+      <label className="custom-label">{labelText}</label>
+
+      <input
+        type="text"
+        className="custom-input"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder={labelText}
+      /><br/>
+      <label className="custom-label">{labelText2}</label>
+
+      <input
+        type="text"
+        className="custom-input"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder={labelText2}
+      />
+    <br/>
+            <label className="inputbox" for="starting_time">Start Time:</label>
                 <input className="inputbox" type="date" id="starting_time"/><br/>
                 <label className="inputbox" for="last_login_time">Last login Time:</label>
                 <input className="inputbox" type="date" id="last_login_time"/><br/>
@@ -25,4 +82,4 @@ const createquiz=()=>{
         </div>
     )
 }
-export default createquiz;
+export default SignUp;
