d8989cab7da4b7c8048b2fe101960db9ddc20874
Prerna <divyadudhekar13@gmail.com>
2023-06-09 11:24:00 +0530

    signup.js

--- client/src/modules/quiz/creator/createQuiz/SignUp.js
+++ client/src/modules/quiz/creator/createQuiz/SignUp.js
@@ -5,15 +5,15 @@ const SignUp=()=>{
         <div id="outer_div">
         <div id="div1">
         <form>
-                <h1>Create Your Own Quiz</h1>
-                <label for="starting_time">Start Time:</label>
+                <h1 className='heading'>Create Your Own Quiz</h1>
+                <label className="inputbox" for="starting_time">Start Time:</label>
                 <input className="inputbox" type="date" id="starting_time"/><br/>
-                <label for="last_login_time">Last login Time:</label>
+                <label className="inputbox" for="last_login_time">Last login Time:</label>
                 <input className="inputbox" type="date" id="last_login_time"/><br/>
-                <label for="Result_time">Result Time:</label>
+                <label className="inputbox" for="Result_time">Result Time:</label>
                 <input className="inputbox" type="date" id="Result_time"/><br/>
-                <p id="question">Question Timer: </p>
-                <select name="question_time">
+                <p id="para">Question Timer: </p>
+                <select id="question" name="question_time">
                 <option selected>Individual Question Timer</option>
                 <option >Section/Full Quiz Timer</option>
         
