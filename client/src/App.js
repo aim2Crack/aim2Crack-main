3011375cd43791b29b9e1fce508e17ddf37f997d
Prerna <divyadudhekar13@gmail.com>
2023-06-09 13:19:21 +0530

    createquizjsx update

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Login from "./modules/user/Login";
 import Summary from "./modules/quiz/creator/Summary";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "./modules/aboutUs/AboutUs";
-import createquiz from "./modules/quiz/creator/createQuiz/createquiz";
+import createquiz from "./modules/quiz/creator/createQuiz/CreateQuiz";
 import AddQuestion from "./modules/quiz/creator/addQuestion/AddQuestion";
 import Navbar from "./components/navbar/Navbar";
 import Summary from "./components/summary/Summary";
@@ -20,7 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/summary" element={<Summary />} />
         <Route path="/addQuestion" element={<AddQuestion />} />
-        <Route path="/createQuiz" element={<SignUp />} />
+        <Route path="/createQuiz" element={<createquiz />} />
         <Route path="/aboutUs" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
--- client/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
+++ client/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
-function CreateQuiz() {
-    
-  return (
-    <div>CreateQuiz</div>
-  )
+const createquiz=()=>{
+    return(
+        <div id="outer_div">
+        <div id="div1">
+        <form>
+                <h1 className='heading'>Create Your Own Quiz</h1>
+                <label className="inputbox" for="starting_time">Start Time:</label>
+                <input className="inputbox" type="date" id="starting_time"/><br/>
+                <label className="inputbox" for="last_login_time">Last login Time:</label>
+                <input className="inputbox" type="date" id="last_login_time"/><br/>
+                <label className="inputbox" for="Result_time">Result Time:</label>
+                <input className="inputbox" type="date" id="Result_time"/><br/>
+                <p id="para">Question Timer: </p>
+                <select id="question" name="question_time">
+                <option selected>Individual Question Timer</option>
+                <option >Section/Full Quiz Timer</option>
+        
+                </select><br></br>
+                <button class="button" type="button">Create Quiz</button>
+                
+            </form>
+   </div>
+        </div>
+    )
 }
-
-export default CreateQuiz
+export default createquiz;
