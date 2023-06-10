4736e1c8d809fa0024ae60c5b3aa813f5344e7a5
Parkhi <parkhigoyal46@gmail.com>
2023-06-10 11:17:18 +0530

    added component links

--- client/src/components/summary/LeftNavigationPanel.js
+++ client/src/components/summary/LeftNavigationPanel.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import '../styles/LeftNavigationPanel.css'
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 const LeftNavigationPanel = () => {
     const [quizDropdown, setQuizDropdown] = useState( false)
@@ -16,22 +16,22 @@ const LeftNavigationPanel = () => {
                 </button>
                 {quizDropdown &&
                     <div className="dropdown-container">
-                    <Link to="/subjective-quiz" id="btn-subjective-quiz">Subjective Quiz</Link>
-                    <Link to="/placement-quiz" id="btn-placement-quiz">Placement Quiz</Link>
+                    <Link to="/summary/subjective-quiz" id="btn-subjective-quiz">Subjective Quiz</Link>
+                    <Link to="/summary/placement-quiz" id="btn-placement-quiz">Placement Quiz</Link>
                     </div>
-
             }
 
 
-                <button className="dropdown-btn side-bar-item">
+                <button className="dropdown-btn side-bar-item" onClick={() => setBookDropdown(!bookDropdown)}>
                     Book
                     <i className="fa fa-caret-down"></i>
                 </button>
 
                 {bookDropdown && <div className="dropdown-container">
-                    <a id="btn-your-books" href="#">Your Books</a>
-                    <a id="btn-your-collections" href="#">Your Collections</a>
+                    <Link to="/summary/your-books" id="btn-your-books">Your Books</Link>
+                    <Link to="/summary/your-collections" id="btn-your-collections">Your Collections </Link>
                 </div>
+                }
 
                 <button className="side-bar-item" id="btn-assignment">
                     Assignments
