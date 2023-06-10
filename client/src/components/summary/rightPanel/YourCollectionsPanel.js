91ad6f901477dcce4b37c0b073a77e44bba89bdb
Parkhi <parkhigoyal46@gmail.com>
2023-06-10 11:25:09 +0530

    completed summary page

--- client/src/components/summary/rightPanel/YourCollectionsPanel.js
+++ client/src/components/summary/rightPanel/YourCollectionsPanel.js
@@ -1,11 +1,29 @@
 import React  from "react";
+import '../../styles/rightpanel.css'
+import searchImage from  '../../../assets/images/summary/searchBar.svg'
+import pdfImage from '../../../assets/images/summary/pdfImage.svg'
 
 const YourCollectionsPanel = ()=>{
     return(
         <div>
+            <div className="search-bar-panel">
+                <input
+                    type="text"
+                    className="search"
+                    name="Search..."
+                    placeholder="Search...."
+                />
+                <input
+                    type="image"
+                    className="search-button-image"
+                    name="search"
+                    src= {searchImage}
+                    alt="Search"
+                />
+            </div>
             <div className="panel-item" id="panel-subjective-quiz">
                 <div className="upload">
-                    <p className="uploaded">SUBJECTIVE QUIZ</p>
+                    <p className="uploaded">Your Collections</p>
                 </div>
                 <div className="btn">
                     <button className="upload-button">
@@ -17,7 +35,7 @@ const YourCollectionsPanel = ()=>{
                     <div className="content-card">
                         <div className="upper-container d-flex">
                             <div className="content-card-icon">
-                                <img src="./images/pdfImage.svg" className="content-card-image"/>
+                                <img src= {pdfImage} className="content-card-image"/>
                             </div>
                             <div className="name-box flex-f-col">
                                 <p className="file-name">Introduction to Machine Learning</p>
@@ -49,3 +67,4 @@ const YourCollectionsPanel = ()=>{
         )
 
 }
+export default YourCollectionsPanel
