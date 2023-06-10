972e8853a75abc7a2ab91697fe02e94396dae301
Parkhi <parkhigoyal46@gmail.com>
2023-06-10 08:48:18 +0530

    updated right panel root

--- client/src/components/summary/rightPanel/RightPanelRoot.js
+++ client/src/components/summary/rightPanel/RightPanelRoot.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import PlacementPanel from './PlacementPanel'
+import {Link, Route, Routes} from "react-router-dom";
 const RightPanelRoot = () =>{
     return(
         <div className='panel'>
-            <PlacementPanel/>
+
+            <Routes>
+                <Route path="/placement-quiz" element={<PlacementPanel/>}/>
+            </Routes>
         </div>
     )
 }
+export default RightPanelRoot
