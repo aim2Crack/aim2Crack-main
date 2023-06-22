70fe030bae52692a7c48ee9acb8284ec4dd35031
Kashish <kashish.1.6jun@gmail.com>
2023-06-22 12:30:02 +0000

    routes are done with create,delete,update and get.

---
+++ server/src/modules/data/controllers/UserController.js
@@ -0,0 +1,78 @@
+const User = require('../models/UserSchema');
+async function createUser(req, res) {
+    try {
+      // Extract user data from the request body
+      const { username, email, phone, password, profile_type } = req.body;
+      
+      // Create a new user using the User model
+      const user = await User.create({ username, email, phone, password, profile_type });
+      
+      // Return the created user in the response
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to create user.' });
+    }
+  }
+  
+  
+
+  // Delete a user by ID
+  async function deleteUser(req, res) {
+    const { id } = req.params;
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found.' });
+      }
+  
+      await user.destroy();
+      res.json({ message: 'User deleted successfully.' });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to delete user.' });
+    }
+  }
+  
+  // Update a user by ID
+  async function updateUser(req, res) {
+    const { id } = req.params;
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found.' });
+      }
+  
+      // Update the user attributes based on the request body
+      user.username = req.body.username;
+      user.email = req.body.email;
+      // Update other attributes as needed
+  
+      await user.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update user.' });
+    }
+  }
+  
+  // Get a user by ID
+  async function getUserById(req, res) {
+    const { id } = req.params;
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found.' });
+      }
+  
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch user.' });
+    }
+  }
+  
+  module.exports = {
+    createUser,
+    deleteUser,
+    updateUser,
+    getUserById,
+  };
+    
+  
---
+++ server/src/modules/data/routes/UserRoutes.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const defineUserModel = require('../models/UserSchema');
+const userController = require('../controllers/UserController');
+
+// Create a new user
+router.post('/users', userController.createUser);
+
+// Get all users
+router.get('/users', userController.getAllUsers);
+
+// Get a user by ID
+router.get('/users/:id', userController.getUserById);
+
+// Update a user by ID
+router.put('/users/:id', userController.updateUser);
+
+// Delete a user by ID
+router.delete('/users/:id', userController.deleteUser);
+
+//new user
+router.post('/users', async (req, res) => {
+    try {
+        const User = await defineUserModel();
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+//get all users
+router.get('/users', async (req, res) => {
+    try {
+        const User = await defineUserModel();
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+//user access by ID
+router.get('/users/:id', async (req, res) => {
+    try {
+        const User = await defineUserModel();
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// update user by ID
+router.put('/users/:id', async (req, res) => {
+    try {
+        const User = await defineUserModel();
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.update(req.body);
+            res.json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// delete user by ID
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const User = await defineUserModel();
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.destroy();
+            res.sendStatus(204);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = router;
