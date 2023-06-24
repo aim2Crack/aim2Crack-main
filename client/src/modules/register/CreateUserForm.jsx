7beee903163a1b7566351bcc1a0ac4282adb4ce4
Jenus <jenusgarg@gmail.com>
2023-06-24 18:24:06 +0530

    Adding Register css files

--- client/src/modules/register/CreateUserForm.jsx
+++ client/src/modules/register/CreateUserForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
-
+import './Register.css';
 const CreateUserForm = () => {
   const navigate = useNavigate();
 
