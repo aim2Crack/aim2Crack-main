c30d4da1a2a4aa5a0cefedc7b084336774bf7847
harimurugan1989 <harimur@gmail.com>
2023-06-25 09:02:49 +0530

    cors updated for aim2crack

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const PORT = 7000
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000 , https://aim2crack.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
