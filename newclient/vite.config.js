bedf119ec822a5b90ec49a81a1ac72b46691f70d
harimurugan1989 <harimur@gmail.com>
2023-07-06 13:30:26 +0530

    Revert "vite config updated"
    This reverts commit a4d2fa6c04a93a7871ba814fa8ee7086197f4cb0.

--- newclient/vite.config.js
+++ newclient/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxInject: `import React from 'react'`,
+  },
   
 })
