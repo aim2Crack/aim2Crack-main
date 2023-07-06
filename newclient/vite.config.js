a4d2fa6c04a93a7871ba814fa8ee7086197f4cb0
harimurugan1989 <harimur@gmail.com>
2023-07-06 13:26:11 +0530

    vite config updated

--- newclient/vite.config.js
+++ newclient/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxInject: `import React from 'react'`,
-  },
+  
   
 })
