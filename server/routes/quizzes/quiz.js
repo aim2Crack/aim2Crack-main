de3f8e1de2afa9b3fdb794d9d15aa0bdb837ed3e
Parkhi <parkhigoyal46@gmail.com>
2023-07-08 07:04:37 +0530

    added quiz routes

---
+++ server/routes/quizzes/quiz.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const Quiz = require('../../models/quiz');
+
+// Create a quiz
+router.post('/quizzes', async (req, res) => {
+    try {
+        const { code, start_time, marginTime, resultTime, quiz_name, section_name, created_on, created_by, updated_on, collaborators } = req.body;
+
+        // Create a new quiz in the database
+        const quiz = await Quiz.create({
+            code,
+            start_time,
+            marginTime,
+            resultTime,
+            quiz_name,
+            section_name,
+            created_on,
+            created_by,
+            updated_on,
+            collaborators
+        });
+
+        res.status(201).json({ success: true, data: quiz });
+    } catch (error) {
+        console.error('Error creating quiz:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
+
+// Get all quizzes
+router.get('/quizzes', async (req, res) => {
+    try {
+        // Fetch all quizzes from the database
+        const quizzes = await Quiz.findAll();
+
+        res.status(200).json({ success: true, data: quizzes });
+    } catch (error) {
+        console.error('Error fetching quizzes:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
+
+//access quiz by id
+router.get('/quizzes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Find a quiz by its ID in the database
+        const quiz = await Quiz.findByPk(id);
+
+        if (quiz) {
+            res.status(200).json({ success: true, data: quiz });
+        } else {
+            res.status(404).json({ success: false, message: 'Quiz not found' });
+        }
+    } catch (error) {
+        console.error('Error fetching quiz:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
+
+// quiz updation
+router.put('/quizzes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { code, start_time, marginTime, resultTime, quiz_name, section_name, created_on, created_by, updated_on, collaborators } = req.body;
+
+        // Find the quiz by ID and update its properties
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) {
+            quiz.code = code;
+            quiz.start_time = start_time;
+            quiz.marginTime = marginTime;
+            quiz.resultTime = resultTime;
+            quiz.quiz_name = quiz_name;
+            quiz.section_name = section_name;
+            quiz.created_on = created_on;
+            quiz.created_by = created_by;
+            quiz.updated_on = updated_on;
+            quiz.collaborators = collaborators;
+
+            await quiz.save();
+
+            res.status(200).json({ success: true, data: quiz });
+        } else {
+            res.status(404).json({ success: false, message: 'Quiz not found' });
+        }
+    } catch (error) {
+        console.error('Error updating quiz:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
+
+//quiz deletion
+router.delete('/quizzes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Find the quiz by ID and delete it
+        const quiz = await Quiz.findByPk(id);
+        if (quiz) {
+            await quiz.destroy();
+            res.status(200).json({ success: true, message: 'Quiz deleted successfully' });
+        } else {
+            res.status(404).json({ success: false, message: 'Quiz not found' });
+        }
+    } catch (error) {
+        console.error('Error deleting quiz:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
