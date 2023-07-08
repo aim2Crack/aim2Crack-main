367154525274d93b9faeab31cfe1933c7186eca6
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2023-07-08 23:54:41 +0530

    Revert "Sign in with google"

--- server/app.js
+++ server/app.js
@@ -2,23 +2,17 @@ const express = require('express')
 const {sq,testDbConnection} = require('./db')
 const {User, ResetPass, Quiz} = require("./models/models");
 const signupRoutes = require('./routes/user/SignUp');
-const firebaseConfig = require('./serviceAccountCredentials.json')
 const resetRoutes= require('./routes/user/passwordReset');
-const admin = require('firebase-admin')
 const mailerRoutes=require('./routes/user/VerifyMailer')
 const loginRoutes= require('./routes/user/login');
 const quizRoutes=require('./routes/quizzes/quiz_faculty/quiz');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express()
 const PORT = 7000
 
-
-
-admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-});
 // CORS middleware
 // CORS middleware
 app.use((req, res, next) => {
@@ -46,7 +40,6 @@ app.options('*', cors({
 require('./auth/auth');
 
 const secureRoute = require('./routes/user/profile');
-const {config} = require("dotenv");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- server/routes/user/login.js
+++ server/routes/user/login.js
@@ -2,99 +2,38 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/user');
 const ResetPass = require('../../models/resetpass');
+
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 // const router = express.Router();
-const {Op} = require('sequelize');
-const mailer = require('./SendMailer')
-const {getAuth} = require("firebase-admin/auth");
-
+const { Op } = require('sequelize');
+const mailer=require('./SendMailer')
 // ...
-const { Client } = require('pg');
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '1234',
-    port: 5432,
-});
-client.connect();
-router.post('/login', passport.authenticate('login', {session: false}), async (req, res, next) => {
+
+router.post(
+  '/login',
+  passport.authenticate('login', { session: false }),
+  async (req, res, next) => {
     try {
-        console.log(req.user.email)
-        console.log(req.user.emailVerify);
-        // verifying email verification if fase use resetpass model data to send mail
-        if (req.user.emailVerify) {
-            const body = {_id: req.user._id, email: req.user.email};
-            const token = jwt.sign({user: body}, 'TOPSECRET');
-            res.json({token});
-        } else {
-            console.log(req.user.email)
-            const resetPass = await ResetPass.findOne({where: {username: req.user.username}});
-            await mailer.sendVerificationEmail(req.user.username, req.user.email);
-            // return res.json('verify email');
-            res.status(403).json({success: false, message: 'Verify Email first. Check Email'});
-        }
+      // console.log(req.user.email)
+      // console.log(req.user.emailVerify);
+      // verifying email verification if fase use resetpass model data to send mail
+      if(req.user.emailVerify){
+        const body = { _id: req.user._id, email: req.user.email };
+        const token = jwt.sign({ user: body },'TOPSECRET');
+        console.log(token);
+        res.json({ token });
+      }else{
+          console.log(req.user.email)
+          const resetPass = await ResetPass.findOne({ where: { username:req.user.username } });
+          mailer.sendVerificationEmail(req.user.username,req.user.email);
+          // return res.json('verify email');
+         res.status(403).json({success:false, message:'Verify Email first. Check Email'});
+          }
     } catch (error) {
-        res.status(401).json({success: false, message: 'login failed'});
+      res.status(401).json({success:false, message:'login failed'});
     }
-});
-
-// router.get(
-//     '/getUser',
-//     async function signInVerify(req, res) {
-//         const token = req.body.accessToken;
-//         console.log(token)
-//         const authResult = await getAuth().verifyIdToken(token)
-//         const userId = authResult.uid
-//         if(userId){
-//             console.log(userId)
-//             const query = await user.find( {
-//                 "uid" : userId
-//             })
-//
-//             res.status(200).json({
-//                 "data" : query
-//             })
-//         }else{
-//             res.status(401).json({message:"Invalid Credentials"})
-//         }
-//     }
-// )
-
-router.get('/getUser', async function signInVerify(req, res) {
-    const token = req.query.accessToken; // Access token from query parameter
-    try {
-        const authResult = await getAuth().verifyIdToken(token);
-        const email = authResult.email;
-
-        if (email) {
-            const query = `SELECT * FROM users WHERE uid = ${email}`;
-            client.query(query, (err, resp) => {
-                if (err) {
-                    console.warn(err);
-                    return;
-                }
-                for (let row of resp.rows) {
-                    console.log(row);
-                }
-                return query
-                // client.end();
-            });
-            res.status(200).json({
-                user: query
-            });
-        } else {
-            res.status(401).json({
-                message: "Invalid Credentials"
-            });
-        }
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        });
-    }
-});
-
+  }
+);
 
 module.exports = router;
