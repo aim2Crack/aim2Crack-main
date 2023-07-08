dc0da075c0c5fcccd39155000a99454b753e7dea
harimurugan1989 <harimur@gmail.com>
2023-07-08 14:05:23 +0530

    quiz sorted

--- newclient/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
+++ newclient/src/modules/quiz/creator/createQuiz/CreateQuiz.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, Redirect } from 'formik';
 import * as Yup from 'yup';
 import './createquiz.css';
 
@@ -14,47 +14,62 @@ const CreateQuiz = () => {
     allowTabchange: false,
   };
   const [message, setMessage] = useState('');
+  const [generatedLink, setGeneratedLink] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (values) => {
-    const token = localStorage.getItem('token');
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      const token = localStorage.getItem('token');
 
-    fetch('http://127.0.0.1:7000/quizzes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(values),
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Request was successful
-          return response.json();
-        } else {
-          // Handle error response
-          console.error('User registration request failed:', response.status);
-          throw new Error('An error occurred during user registration.');
-        }
-      })
-      .catch((error) => {
-        // Handle any network or other errors
-        console.error('User registration request failed:', error);
-        setMessage('An error occurred during user registration.');
-        setSubmitted(false);
+      // Submit the data to the backend
+      const response = await fetch('http://127.0.0.1:7000/quizzes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(values),
       });
+
+      if (response.ok) {
+        // Request was successful
+      const data = await response.json();
+      const allquiz = data.data;
+      console.log(allquiz.code);
+      // Access the necessary details from the updated allquiz array
+      const generatedLink = allquiz.code;
+
+        // Set the generated link in the component state
+        // setMessage(`Quiz created successfully. Redirecting to the quiz page...`);
+          // Set the generated code in the component state
+     setMessage(`Quiz created successfully. Redirecting to the quiz page...`);
+     setGeneratedLink(generatedLink);
+     setSubmitted(true);
+      } else {
+        // Handle error response
+        console.error('Error creating quiz:', response.status);
+        setMessage('An error occurred during quiz creation.');
+      }
+    } catch (error) {
+      console.error('Error creating quiz:', error);
+      setMessage('An error occurred during quiz creation.');
+    }
+
+    setSubmitting(false);
   };
 
   const validationSchema = Yup.object().shape({
     quizName: Yup.string().required('Quiz name is required'),
     startTime: Yup.date().required('Start time is required'),
     marginTime: Yup.date().required('Last login time is required'),
-    resultTime: Yup.date()
-      .required('Result time is required')
-      .min(Yup.ref('startTime'), 'Result time must be greater than start time'),
+    resultTime: Yup.date().required('Result time is required').min(Yup.ref('startTime'), 'Result time must be greater than start time'),
     negativeMarking: Yup.number().required('Negative marking is required'),
   });
 
+  if (submitted && generatedLink) {
+    const redirectTo = `/addquestion/${generatedLink}`;
+    return <Redirect to={redirectTo} />;
+  }
   return (
     <div id="outer_div">
       <div id="div1">
