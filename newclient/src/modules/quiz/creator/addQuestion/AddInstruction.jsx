9097653a414adb66111b73678a1c31ad6cfbd8ee
harimurugan1989 <harimur@gmail.com>
2023-07-11 23:55:00 +0530

    add instruction page linked with backend

--- newclient/src/modules/quiz/creator/addQuestion/AddInstruction.jsx
+++ newclient/src/modules/quiz/creator/addQuestion/AddInstruction.jsx
@@ -12,44 +12,64 @@ export default function AddInstruction() {
   const [inputValue, setInputValue] = useState('');
   const [storedValues, setStoredValues] = useState([]);
   const [submitted, setSubmitted] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = async (values, { setSubmitting }) => {
+  const handleSubmit = async () => {
     try {
       const token = localStorage.getItem('token');
-      const code = window.location.pathname.split('/').pop();
-      // Submit the data to the backend
-      const response = await fetch(`http://127.0.0.1:7000/quizzes/${code}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(values),
-      }); 
-       } catch (error) {
-      console.error('Error submitting instriuction:', error);
-      setMessage('An error occurred during submission.');
-    }
+      console.log(token);
+      const code = window.location.pathname.split('/').filter(path => path !== 'addinstruction').pop();
+      console.log(code);
+      const lines = inputValue.split('\n').filter((line) => line.trim() !== '');
+  
+      if (lines.length > 0) {
+        const instructions = lines.map((line) => line.trim());
+        const values = {
+          instructions: instructions,
+        };
+        // Submit the data to the backend
+        const response = await fetch(`http://127.0.0.1:7000/quizzes/${code}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(values),
+        }); 
+        if (response.status === 200) {
+          setSubmitted(true);
+          setMessage('Instructions updated successfully');
 
+        } else {
+          console.error('Error submitting instructions:', response);
+          // setMessage('An error occurred during submission.');
+        }
+      }
+  
+    } catch (error) {
+      console.error('Error submitting instructions:', error);
+      // setMessage('An error occurred during submission.');
+    }
+  
     setSubmitting(false);
   };
-
-
+  
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   // stroing instruction as array of strings
-  const handleStoreValue = () => {
-    const lines = inputValue.split('\n').filter((line) => line.trim() !== '');
+  // const handleStoreValue = () => {
+  //   const lines = inputValue.split('\n').filter((line) => line.trim() !== '');
   
-    if (lines.length > 0) {
-      const instructions = lines.map((line) => line.trim());
-      setStoredValues([...storedValues, ...instructions]);
-      setInputValue('');
-    }
-  };
+  //   if (lines.length > 0) {
+  //     const instructions = lines.map((line) => line.trim());
+  //     setStoredValues([...storedValues, ...instructions]);
+  //     setInputValue('');
+  //   }
+  // };
   
     
   return (
@@ -66,7 +86,8 @@ export default function AddInstruction() {
           onChange={handleInputChange}
         />
       </form>
-      <button className="submitb" onClick={handleStoreValue}>Update Instructions</button>
+      <button className="submitb" onClick={handleSubmit}>Update Instructions</button>
+      {message && <p>{message}</p>}
       <button className="submitb"  >Preview</button>
 
     </div>
