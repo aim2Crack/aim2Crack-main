a1197f59e803b292138249faaa2f9f50f716afb6
harimurugan1989 <harimur@gmail.com>
2023-07-19 11:58:27 +0530

    vefiry mailer updaed

--- newclient/src/components/navbar/Navbar.jsx
+++ newclient/src/components/navbar/Navbar.jsx
@@ -11,61 +11,61 @@ function Navbar() {
 
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // Check if the user is logged in
-    const isLoggedIn = checkUserLoggedIn(); // Replace with your logic to check if the user is logged in
+  // useEffect(() => {
+  //   // Check if the user is logged in
+  //   const isLoggedIn = checkUserLoggedIn(); // Replace with your logic to check if the user is logged in
 
-    if (isLoggedIn) {
-      const userName = getLoggedInUserName(); // Replace with your logic to get the logged-in user's name
-      setUser({ displayName: userName });
-    } else {
-      setUser(null);
-    }
-  }, []);
+  //   if (isLoggedIn) {
+  //     const userName = getLoggedInUserName(); // Replace with your logic to get the logged-in user's name
+  //     setUser({ displayName: userName });
+  //   } else {
+  //     setUser(null);
+  //   }
+  // }, []);
 
-  const checkUserLoggedIn = () => {
-    // Implement your logic to check if the user is logged in
-    // Return true if the user is logged in, false otherwise
-    // Example: You can check if a token is present in local storage or if the user is authenticated with your authentication system
-    const token = localStorage.getItem('token');
-    return !!token;
-  };
+  // const checkUserLoggedIn = () => {
+  //   // Implement your logic to check if the user is logged in
+  //   // Return true if the user is logged in, false otherwise
+  //   // Example: You can check if a token is present in local storage or if the user is authenticated with your authentication system
+  //   const token = localStorage.getItem('token');
+  //   return !!token;
+  // };
 
-  const getLoggedInUserName = () => {
-    // Implement your logic to get the logged-in user's name
-    // Return the user's name if available, or an empty string otherwise
-    // Example: You can decode the JWT token and extract the user's name from it
-    const token = localStorage.getItem('token');
-    // Decode the token and extract the user's name
-    const decodedToken = decodeToken(token);
-    return decodedToken ? decodedToken.name : '';
-  };
+  // const getLoggedInUserName = () => {
+  //   // Implement your logic to get the logged-in user's name
+  //   // Return the user's name if available, or an empty string otherwise
+  //   // Example: You can decode the JWT token and extract the user's name from it
+  //   const token = localStorage.getItem('token');
+  //   // Decode the token and extract the user's name
+  //   const decodedToken = decodeToken(token);
+  //   return decodedToken ? decodedToken.name : '';
+  // };
 
-  const decodeToken = (token) => {
-    // Implement your logic to decode the JWT token
-    // Example: You can use a library like jwt.io or jsonwebtoken to decode the token
-    // Return the decoded token object or null if decoding fails
-    // Example using jsonwebtoken library:
-    try {
-      const decodedToken = jwt.decode(token);
-      return decodedToken;
-    } catch (error) {
-      console.error('Error decoding token:', error);
-      return null;
-    }
-  };
+  // const decodeToken = (token) => {
+  //   // Implement your logic to decode the JWT token
+  //   // Example: You can use a library like jwt.io or jsonwebtoken to decode the token
+  //   // Return the decoded token object or null if decoding fails
+  //   // Example using jsonwebtoken library:
+  //   try {
+  //     const decodedToken = jwt.decode(token);
+  //     return decodedToken;
+  //   } catch (error) {
+  //     console.error('Error decoding token:', error);
+  //     return null;
+  //   }
+  // };
 
-  const handleLogout = () => {
-    // Implement your logic to handle the logout action
-    // Example: You can clear the token from local storage or perform any necessary cleanup
-    localStorage.removeItem('token');
-    setUser(null);
-    window.location.href = '/login'; // Redirect to the login page
-  };
+  // const handleLogout = () => {
+  //   // Implement your logic to handle the logout action
+  //   // Example: You can clear the token from local storage or perform any necessary cleanup
+  //   localStorage.removeItem('token');
+  //   setUser(null);
+  //   window.location.href = '/login'; // Redirect to the login page
+  // };
 
-  if (isExcludedPath) {
-    return null; // Render nothing if the current path is excluded
-  }
+  // if (isExcludedPath) {
+  //   return null; // Render nothing if the current path is excluded
+  // }
 
   const openNav = () => {
     // Implement your logic for opening the navigation menu
