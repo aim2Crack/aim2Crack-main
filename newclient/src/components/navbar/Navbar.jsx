74f862e4c28c7d92c724ff6f60598f06fefe5ba0
harimurugan1989 <harimur@gmail.com>
2023-07-30 11:39:00 +0530

    login required completed

--- newclient/src/components/navbar/Navbar.jsx
+++ newclient/src/components/navbar/Navbar.jsx
@@ -2,36 +2,69 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import '../styles/Navbar.css';
 // import jwt from 'jsonwebtoken';
-import aim2CrackLogo from '../../assets/images/navbar/Aim2Crack-logo.png';
+// import aim2CrackLogo from '../../assets/images/navbar/Aim2Crack-logo.png';
 
 function Navbar() {
   const code = window.location.pathname.split('/')[2];
-  console.log(code);
+  // console.log(code);
   const navigate = useNavigate();
 
   const excludedPaths = [
     '/register',
+    '/login',
     '/forgot-password',
     `/quiz/${code}/test`,
     `/quiz/${code}/live`,
     `/quiz/${code}/feedback`
   ];
 
-  const [user, setUser] = useState(null);
-  console.log(user);
+  // const [user, setUser] = useState(null);
+  const [tokenState,setTokenState]=useState(false);
+  // const [loading, setLoading] = useState(true); // Add loading state
+
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    // Function to check the login status
+    const checkLoginStatus = async () => {
+      if (!token) {
+        // No token, navigate to login
+        navigate('/login');
+        // setTokenState(false);
+      } else {
+        // Token found, set tokenState to true
+        setTokenState(true);
+      }
+      // Done checking, set loading to false
+    };
+
+    checkLoginStatus();
+  }, [token]);
+  
+// // Add another useEffect to update tokenState when the token changes
+// useEffect(() => {
+//   setTokenState(Boolean(token));
+// }, [token]);
+
+
+
   const location = useLocation();
   const isExcludedPath = excludedPaths.includes(location.pathname);
   if (isExcludedPath) {
     return null; // Render nothing if the current path is excluded
   }
 
-  // Redirect to "/login" using useNavigate hook if the user is not logged in (user === null)
-  if (!user) {
-    navigate('/login');
-    return null; // Return null or a loading component since the user is being redirected
-  }
-  const openNav = () => {
+    const openNav = () => {
     // Implement your logic for opening the navigation menu
+  };
+
+  const handleLogout = () => {
+    // Implement the logout logic here
+    // Clear the token from localStorage and update the user state if needed
+    localStorage.removeItem('token');
+    // setUser(null); // If you are using the `user` state, reset it to null upon logout
+    setTokenState(false); // Update tokenState to false to indicate the user is logged out
+    navigate('/login') // Redirect the user to the login page after logout
   };
 
   return (
@@ -52,14 +85,14 @@ function Navbar() {
           <li className="dropdown">
             {/* ... About dropdown contents ... */}
           </li>
-          {user && (
+          {tokenState && (
             <li className="user-info">
-              <span className="material-icons">person</span>
-              <span className="user-name">{user.displayName}</span>
+              <span className="material-icons"></span>
+              {/* <span className="user-name">{user.displayName}</span> */}
               <button onClick={handleLogout}>Logout</button>
             </li>
           )}
-          {!user && (
+          {!tokenState && (
             <li>
               <a href="/login">Login</a>
             </li>
