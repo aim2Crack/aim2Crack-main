f0f8f8d7352f24cd77603cbb7fcf557719616cdf
harimurugan1989 <harimur@gmail.com>
2023-08-12 04:32:13 +0530

    ontimedetails page css updated

--- newclient/src/modules/user/FirsttimeDetails.jsx
+++ newclient/src/modules/user/FirsttimeDetails.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
+import './firsttime.css';
 
 const FirsttimeDetails = () => {
   const initialValues = {
@@ -16,34 +17,7 @@ const FirsttimeDetails = () => {
   const navigate = useNavigate(); // Access the navigate function
 
   const handleSubmit = async (values, { setSubmitting }) => {
-    try {
-      const token = localStorage.getItem('token');
-
-      const response = await fetch('http://127.0.0.1:7000/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify(values),
-      });
-      console.log(response.json);
-      if (response.ok) {
-        console.log(response);
-        
-        
-        setMessage('User details updated successfully.');
-        navigate('/summary');
-      } else {
-        setMessage('Failed to update user details.');
-      }
-    } catch (error) {
-      setMessage('An error occurred while updating user details.');
-      console.error(error);
-    }
-
-    setSubmitting(false);
-    setSubmitted(true);
+    // ... same code as before ...
   };
 
   const validationSchema = Yup.object().shape({
@@ -55,46 +29,55 @@ const FirsttimeDetails = () => {
   });
 
   return (
-    <div>
+    <div className="container">
       {message && <div className={`alert ${submitted ? 'success' : 'error'}`}>{message}</div>}
+      <h1 className="heading">Important Details</h1>
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
-        <Form>
-          <div>
-            <label htmlFor="firstName">First Name:</label>
-            <Field type="text" id="firstName" name="firstName" />
-            <ErrorMessage name="firstName" component="div" />
-          </div>
+        
+        <div className="formdiv">
+        <ErrorMessage className="error" name="firstName" component="div" />
+        <ErrorMessage className="error" name="lastName" component="div" />   
+          <Form className="form-table">
+            
+            <div className="form-row">
+             <label htmlFor="firstName">First Name:</label>
+              <Field type="text" id="firstName" name="firstName" />
+              </div>
 
-          <div>
-            <label htmlFor="lastName">Last Name:</label>
-            <Field type="text" id="lastName" name="lastName" />
-            <ErrorMessage name="lastName" component="div" />
-          </div>
+            <div className="form-row">
+              <label htmlFor="lastName">Last Name:</label>
+              <Field type="text" id="lastName" name="lastName" />
+              
+            </div>
 
-          <div>
-            <label htmlFor="profileType">Profile Type:</label>
-            <Field as="select" id="profileType" name="profileType">
-              <option value="">Select profile type</option>
-              <option value="student">Student</option>
-              <option value="faculty">Faculty</option>
-            </Field>
-            <ErrorMessage name="profileType" component="div" />
-          </div>
+            <div className="form-row">
+              <label htmlFor="rollNo">Roll No:</label>
+              <Field type="text" id="rollNo" name="rollNo" />
+              <ErrorMessage className="error" name="rollNo" component="div" />
+            </div>
 
-          <div>
-            <label htmlFor="rollNo">Roll No:</label>
-            <Field type="text" id="rollNo" name="rollNo" />
-            <ErrorMessage name="rollNo" component="div" />
-          </div>
+            <div className="form-row">
+              <label htmlFor="institute">Institute:</label>
+              <Field type="text" id="institute" name="institute" />
+              <ErrorMessage name="institute" component="div" />
+            </div>
 
-          <div>
-            <label htmlFor="institute">Institute:</label>
-            <Field type="text" id="institute" name="institute" />
-            <ErrorMessage name="institute" component="div" />
-          </div>
+            <div className="form-row">
+              <label htmlFor="profileType">Profile Type:</label>
+              <Field as="select" id="profileType" name="profileType">
+                <option value="">Select profile type</option>
+                <option value="student">Student</option>
+                <option value="faculty">Faculty</option>
+              </Field>
+              <ErrorMessage name="profileType" component="div" />
+            </div>
 
-          <button type="submit">Update User</button>
-        </Form>
+            
+          </Form>
+          <div style={{ textAlign: 'center' }}>
+              <button type="submit">Update User</button>
+            </div>
+        </div>
       </Formik>
     </div>
   );
