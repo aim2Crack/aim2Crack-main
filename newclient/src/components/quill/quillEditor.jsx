1e02990bc3572361769759b0bad42785753fd748
harimurugan1989 <harimur@gmail.com>
2023-09-09 04:46:34 +0530

    image function removed

--- newclient/src/components/quill/quillEditor.jsx
+++ newclient/src/components/quill/quillEditor.jsx
@@ -3,37 +3,6 @@ import 'react-quill/dist/quill.snow.css';
 import { useRef } from 'react';
 
 // Custom image handler for Quill
-function imageHandler(quillRef) {
-  // const quillRef = useRef(null);
-
-  const input = document.createElement('input');
-  input.setAttribute('type', 'file');
-  input.setAttribute('accept', 'image/*');
-  input.click();
-  input.onchange = async () => {
-    const file = input.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-
-    const response = await fetch('http://127.0.0.1:7000/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      const responseData = await response.json();
-      const serverUrl = 'http://127.0.0.1:7000/upload'; // Your server URL
-      const imageUrl = new URL(responseData.path, serverUrl).href;
-      console.log (imageUrl)
-      const editor = quillRef.current.getEditor();
-      const range = editor.getSelection(true);
-      editor.insertEmbed(range.index, 'image', imageUrl);
-    } else {
-      console.error('Image upload failed:', response.status);
-    }
-  }
-}
-
 const QuillEditor = ({ value, onChange }) => {
   const quillRef = useRef(null);
 
@@ -41,14 +10,12 @@ const QuillEditor = ({ value, onChange }) => {
     toolbar: {
       container: [
         [{ header: [1, 2, 3, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        ['font', 'size'],
+        ['bold', 'italic'],
         [{ list: 'ordered' }, { list: 'bullet' }],
         ['link', 'image'], // Include the 'image' button in the toolbar
         ['clean'],
       ],
-      // handlers: {
-      //   image: () => imageHandler(quillRef),// Handle image uploads
-      // },
     },
   };
 
