6fa774aa36a13fdb08fe52e39a4eb77e54bcad6d
harimurugan1989 <harimur@gmail.com>
2023-09-28 18:02:31 +0530

    sourcemap enabled

--- newclient/vite.config.js
+++ newclient/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    jsxFactory: 'React.createElement',
+    // jsxFactory: 'React.createElement',
     // jsxInject: `import React from 'react'`,
+  },
+  build: {
+    sourcemap: true,
   },
 
 })
