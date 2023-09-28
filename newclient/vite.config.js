eff2b840581f361f452ac723688533ce2ad2c06c
harimurugan1989 <harimur@gmail.com>
2023-09-28 06:33:21 +0530

    chucks minified made false

--- newclient/vite.config.js
+++ newclient/vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
     jsxFactory: 'React.createElement',
-    // jsxInject: `import React from 'react'`,
   },
   assetsInlineLimit: 0,
   optimizeDeps: {
     exclude: ['@fortawesome/fontawesome-svg-core'],
-    // include: ['xlsx'],
   },
-})
+  build: {
+    // Add the rollupOptions section for manualChunks
+    minify:'false',
+    rollupOptions: {
+      // Define manualChunks to customize chunk splitting
+      manualChunks(id) {
+        // Define your custom logic here to group modules into chunks.
+        // For example:
+        if (id.includes('node_modules')) {
+          return 'vendor'; // Place modules from node_modules in a 'vendor' chunk.
+        }
+      },
+    },
+  },
+});
